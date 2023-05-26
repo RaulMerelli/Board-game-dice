a727ce39822ebf7e5e807b60a63046dd967bc93d
Raul Merelli <raul.merelli@hotmail.it>
2023-05-27 01:27:48 +0200

    Added support for multiple dices, bug fixes

--- script.js
+++ script.js
@@ -1,32 +1,130 @@
 "use strict";
 
-const cube = document.querySelector('.cube');
-const container = document.querySelector('.container');
 const body = document.querySelector('body');
-let rotationIncrement = 0;
+const board = document.querySelector('.board');
+const optionsContainer = document.querySelector('.option-container');
+const numberOfDicesContainer = document.querySelector('.number-of-dices-container');
 const rotationDecrement = 1.01; // Speed decrement
+const initialRotationIncrement = 24;
 const throwingEnabled = true;
 const movingEnabled = true;
-let animationId = null;
-let currentRotationX = 0;
-let currentRotationY = 0;
-let currentRotationZ = 0;
-let currentTranslateX = 0;
-let currentTranslateY = 0;
-let startTranslateX = 0;
-let startTranslateY = 0;
+var numberOfDices = 1;
 let isAnimating = false;
-let ratio = 0;
 
-body.addEventListener('click', handleCubeClick);
+board.addEventListener('click', handleCubeClick);
+optionsContainer.addEventListener('click', handleOptionClick);
+numberOfDicesContainer.querySelector('.more').addEventListener('click', moreDicesClick);
+numberOfDicesContainer.querySelector('.less').addEventListener('click', lessDicesClick);
+
+function moreDicesClick() {
+  if (numberOfDices<10) {
+    numberOfDices++;
+    numberOfDicesContainer.querySelector('.label').innerHTML = numberOfDices;
+  }
+}
+
+function lessDicesClick() {
+  if (numberOfDices>0) {
+    numberOfDices--;
+    numberOfDicesContainer.querySelector('.label').innerHTML = numberOfDices;
+  }
+}
+
+function handleOptionClick() {
+  const menu = document.getElementsByClassName("menu")[0];
+  if (menu.classList.contains("menu-opened"))
+  {
+    menu.getElementsByClassName("arrow")[0].classList.remove('rotate');
+    menu.classList.remove("menu-opened");
+    menu.classList.add("menu-closed");
+  } 
+  else if (menu.classList.contains("menu-closed"))
+  {
+    menu.getElementsByClassName("arrow")[0].classList.add('rotate');
+    menu.classList.remove("menu-closed");
+    menu.classList.add("menu-opened");
+  }
+}
 
 function handleCubeClick() {
-  if (!isAnimating) {
-    rotationIncrement = 24; // Set rotation speed
-    isAnimating = true;
-    const facesToRotate = calculateFacesToRotate();
-    animate(facesToRotate);
+  
+  var containers = document.getElementsByClassName("container");
+  var numContainers = containers.length;
+
+  for (let i = 0; i < numContainers; i++) {
+    const element = containers[i];
+    if (element.isAnimating) {
+      return; // Abort if any animation is already running
+    }
   }
+  
+  for (let i = containers.length - 1; i > numberOfDices; i--) {
+    containers[i].remove();
+  }
+
+  for (let i = numContainers; i<=numberOfDices; i++)
+  {
+    const divOrigin = document.getElementsByClassName("container")[0];
+    const divClone = divOrigin.cloneNode(true);
+    divClone.classList.remove("hidden");
+    board.appendChild(divClone);
+  }
+
+  containers = document.getElementsByClassName("container");
+  numContainers = containers.length;
+  const targetPositions = generateTargetPositions(numContainers);
+
+  for (let i = 0; i < numContainers; i++) {
+    const element = containers[i];
+    const targetPosition = targetPositions[i];
+    const facesToRotate = calculateFacesToRotate();
+    element.isAnimating = true;
+    animate(facesToRotate, element, targetPosition);
+  }
+}
+
+function generateTargetPositions(numContainers) {
+  const targetPositions = [];
+  const containerWidth = document.getElementsByClassName("container")[0].offsetWidth;
+  const containerHeight = document.getElementsByClassName("container")[0].offsetHeight;
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  for (let i = 0; i <= numContainers; i++) {
+    let targetTranslateX, targetTranslateY;
+    let overlapping = false;
+
+    do {
+      targetTranslateX = Math.floor(Math.random() * (windowWidth - containerWidth*2)+containerWidth);
+      targetTranslateY = Math.floor(Math.random() * (windowHeight - containerHeight*2)+containerHeight);
+      overlapping = checkOverlap(targetTranslateX, targetTranslateY, containerWidth, containerHeight, targetPositions, i);
+    } while (overlapping);
+
+    targetPositions.push({ x: targetTranslateX, y: targetTranslateY });
+  }
+
+  return targetPositions;
+}
+
+function checkOverlap(targetTranslateX, targetTranslateY, containerWidth, containerHeight, targetPositions, currentIndex) {
+  for (let i = 0; i < currentIndex; i++) {
+    const position = targetPositions[i];
+    const left1 = targetTranslateX;
+    const right1 = targetTranslateX + containerWidth;
+    const top1 = targetTranslateY;
+    const bottom1 = targetTranslateY + containerHeight;
+
+    const left2 = position.x;
+    const right2 = position.x + containerWidth;
+    const top2 = position.y;
+    const bottom2 = position.y + containerHeight;
+
+    if (left1 < right2 && right1 > left2 && top1 < bottom2 && bottom1 > top2) {
+      return true; // Overlapping detected
+    }
+  }
+
+  return false; // No overlapping
 }
 
 function calculateFacesToRotate() {
@@ -40,7 +138,19 @@ function calculateFacesToRotate() {
   };
 }
 
-function animate(facesToRotate) {
+function animate(facesToRotate, container, targetPosition) {
+  let rotationIncrement = initialRotationIncrement;
+  let currentRotationX = 0;
+  let currentRotationY = 0;
+  let currentRotationZ = 0;
+  let currentTranslateX = 0;
+  let currentTranslateY = 0;
+  let startTranslateX = 0;
+  let startTranslateY = 0;
+  let ratio = 0;
+  let animationId = 0;
+  const cube = container.querySelector('.cube');
+
   const targetRotationX = currentRotationX + facesToRotate.x * 90;
   var targetRotationY = (currentRotationY + facesToRotate.y * 90);
   var targetRotationZ = (currentRotationZ + facesToRotate.z * 90);
@@ -54,8 +164,9 @@ function animate(facesToRotate) {
     currentRotationZ += Math.ceil(Math.random() * 85);
     targetRotationY += 360 - currentRotationY%360;
   }
-  const targetTranslateX = Math.floor(Math.random() * (window.innerWidth - 200)) + 100;
-  const targetTranslateY = Math.floor(Math.random() * (window.innerHeight - 200)) + 100;
+
+  const targetTranslateX = targetPosition.x;
+  const targetTranslateY = targetPosition.y;
   startTranslateX = container.offsetLeft;
   startTranslateY = container.offsetTop;
 
@@ -70,13 +181,13 @@ function animate(facesToRotate) {
     } else if (facesToRotate.z > facesToRotate.y && facesToRotate.z > facesToRotate.x) {
       current = currentRotationZ;
       target = targetRotationZ;
-    } else  {
+    } else {
       current = currentRotationY;
       target = targetRotationY;
     }
     ratio = current / target;
-    var translateZ = throwingEnabled?(ratio*-2000.0)+800:-1200;
-    if (movingEnabled) 
+    var translateZ = throwingEnabled ? (ratio * -2000.0) + 800 : -1200;
+    if (movingEnabled)
       animateMovement();
     if (currentRotationX < targetRotationX) currentRotationX += rotationIncrement;
     if (currentRotationY < targetRotationY) currentRotationY += rotationIncrement;
@@ -94,19 +205,15 @@ function animate(facesToRotate) {
       currentRotationY = targetRotationY % 360;
       currentRotationZ = targetRotationZ % 360;
       cube.style.transform = `translateZ(${translateZ}px) rotateX(${currentRotationX}deg) rotateY(${currentRotationY}deg) rotateZ(${currentRotationZ}deg)`;
-      isAnimating = false;
-      // Delay before allowing subsequent clicks
-      setTimeout(() => {
-        isAnimating = false;
-      }, 100);
+      container.isAnimating = false;
     }
 
-      // Gradually decrease rotation speed
-      if (rotationIncrement / rotationDecrement > 16) {
-        rotationIncrement /= rotationDecrement;
-      }
+    // Gradually decrease rotation speed
+    if (rotationIncrement / rotationDecrement > 16) {
+      rotationIncrement /= rotationDecrement;
+    }
   }
-  
+
   function animateMovement() {
     const translateXDiff = targetTranslateX - startTranslateX;
     const translateYDiff = targetTranslateY - startTranslateY;
